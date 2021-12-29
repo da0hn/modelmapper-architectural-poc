bd16d76a37fe2d57432c8e34f6ea7ad6a522416d
Gabriel Honda <gabrielhondacba@gmail.com>
2021-12-29 19:03:42 -0400

    Initial commit

---
+++ src/main/java/org/da0hn/poc/ModelmapperArchitecturalPocApplication.java
@@ -0,0 +1,13 @@
+package org.da0hn.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelmapperArchitecturalPocApplication {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(ModelmapperArchitecturalPocApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/da0hn/poc/ModelmapperArchitecturalPocApplicationTests.java
@@ -0,0 +1,13 @@
+package org.da0hn.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelmapperArchitecturalPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
